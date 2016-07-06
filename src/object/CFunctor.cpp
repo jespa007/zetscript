bea4feca4f038bda534e238d6a13655cfb5967e2
jespa007 <jespada@jespada-Inspiron-N5050>
2016-07-06 23:55:10 +0200

    -added CFunctor *

---
+++ src/object/CFunctor.cpp
@@ -0,0 +1,7 @@
+#include "script/zg_script.h"
+
+
+CFunctor::CFunctor(){
+	m_varType =VAR_TYPE::FUNCTION;
+	m_ptr=NULL;
+}
---
+++ src/object/CFunctor.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class CFunctor:public CVariable{
+
+public:
+
+	CFunctor();
+
+};
