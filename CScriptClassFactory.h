cdabbcef3ddbf8f8d018c71ad60b839be46449d2
Jordi E <jordi@braingaze.eu>
2019-07-04 18:28:52 +0200

    Implementing #120 (not finished yet)

--- CScriptClassFactory.h
+++ CScriptClassFactory.h
@@ -9,18 +9,9 @@
 
 #define ZS_INVALID_CLASS						((unsigned char)ZS_UNDEFINED_IDX)
 
-#define REGISTER_C_FUNCTION(text,s) 			zs::CScriptClassFactory::setFilenameLine(__FILE__, __LINE__);zs::CScriptClassFactory::getInstance()->register_C_FunctionInt(text,s)
-#define REGISTER_C_VARIABLE(text,s) 			zs::CScriptClassFactory::setFilenameLine(__FILE__, __LINE__);zs::CScriptClassFactory::getInstance()->register_C_VariableInt(text,&s,typeid(decltype(&s)).name())
 
-#define REGISTER_C_CLASS 						zs::CScriptClassFactory::setFilenameLine(__FILE__, __LINE__);zs::CScriptClassFactory::getInstance()->register_C_ClassInt
-#define REGISTER_C_SINGLETON_CLASS				zs::CScriptClassFactory::setFilenameLine(__FILE__, __LINE__);zs::CScriptClassFactory::getInstance()->register_C_SingletonClassInt
-
-#define REGISTER_C_VARIABLE_MEMBER				zs::CScriptClassFactory::setFilenameLine(__FILE__, __LINE__);zs::CScriptClassFactory::getInstance()->register_C_VariableMemberInt
-#define REGISTER_C_FUNCTION_MEMBER				zs::CScriptClassFactory::setFilenameLine(__FILE__, __LINE__);zs::CScriptClassFactory::getInstance()->register_C_FunctionMemberInt //<o>(s,&f)
 #define CLASS_C_BASEOF							zs::CScriptClassFactory::getInstance()->class_C_BaseOfInt //<o>(s,&f)
 
-
-#define REGISTER_C_STATIC_FUNCTION_MEMBER		zs::CScriptClassFactory::setFilenameLine(__FILE__, __LINE__);zs::CScriptClassFactory::getInstance()->register_C_StaticFunctionMemberInt
 
 #define NEW_CLASS_VAR_BY_IDX(idx) 				(zs::CScriptClassFactory::getInstance()->instanceScriptVariableByIdx(idx))
 
@@ -141,57 +132,7 @@ namespace zs{
 
 
 
-			/**
-			 * REGISTER C Stuff
-			 */
 
-			/**
-			 * Register C function
-			 */
-			template <typename F>
-			static bool register_C_FunctionInt( const char * function_name,F function_ptr);
-
-			/**
-			 * Register C variable
-			 */
-			 tVariableSymbolInfo * register_C_VariableInt(const std::string & var_str,void * var_ptr, const std::string & var_type);
-			 unsigned char getIdx_C_RegisteredClass(const std::string & str_classPtr);
-
-
-			/**
-			 * Register C Class. Return index registered class
-			 */
-			template<class _T>
-			 CScriptClass * register_C_SingletonClassInt(const std::string & class_name);
-
-			/**
-			 * Register C Class. Return index registered class
-			 */
-			template<class _T>
-			CScriptClass * register_C_ClassInt(const std::string & class_name);
-
-
-			template<class _T, class _B>
-			bool class_C_BaseOfInt();
-
-			/**
-			 * Register C Member function Class
-			 */
-			template < typename _C, typename _R, class _T, typename..._A>
-			bool register_C_FunctionMemberInt(const char *function_name,_R (_T:: *function_type)(_A...) );
-
-			/**
-			 * Register C Member function Class
-			 */
-			template <typename _T, typename _F>
-			bool register_C_StaticFunctionMemberInt(const char *function_name,_F function_type);
-
-
-			/**
-			 * Register C Member var
-			 */
-			template <typename _C, typename _R,typename _T>
-			bool register_C_VariableMemberInt(const char *var_name, _R _T::*var_pointer);
 
 
 		private:
