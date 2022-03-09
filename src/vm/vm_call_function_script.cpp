b64e1fd333b7d5ad9ab4c3e468672fe370f7e770
jespada <jespa007@gmail.com>
2022-03-09 19:54:39 +0100

    last changes

--- src/vm/vm_call_function_script.cpp
+++ src/vm/vm_call_function_script.cpp
@@ -513,11 +513,107 @@ find_element_object:
 				}
 
 				// store through metamethod
-				if((STK_IS_SCRIPT_OBJECT_CLASS(stk_dst) && ((store_lst_setter_functions=((ScriptObjectClass *)stk_dst->value)->getSetterList(BYTE_CODE_METAMETHOD_SET))!=NULL))
-						||
-					(stk_dst->properties & STK_PROPERTY_MEMBER_PROPERTY)){
-					so_aux=(ScriptObjectClass *)stk_dst->value;
-					VM_SET_METAMETHOD(stk_dst,stk_src, so_aux, store_lst_setter_functions);
+				store_lst_setter_functions=NULL;
+
+				if(STK_IS_SCRIPT_OBJECT_CLASS(stk_dst)){
+					if((store_lst_setter_functions=((ScriptObjectClass *)stk_dst->value)->getSetterList(BYTE_CODE_METAMETHOD_SET))!=NULL){
+
+						if(store_lst_setter_functions->count == 0){
+							store_lst_setter_functions=NULL;
+						}
+					}
+				}else if(stk_dst->properties & STK_PROPERTY_MEMBER_PROPERTY){
+					StackMemberProperty *	stk_mp=(StackMemberProperty *)stk_dst->value;\
+					if(stk_mp->member_property->metamethod_members.setters.count > 0){\
+						store_lst_setter_functions=&stk_mp->member_property->metamethod_members.setters;\
+					}\
+				}
+
+				if(store_lst_setter_functions!=NULL){
+
+					//VM_SET_METAMETHOD(stk_dst,stk_src, so_aux, store_lst_setter_functions);
+					StackElement *stk_vm_start=data->stk_vm_current;\
+					StackElement *stk_arg=stk_vm_start+1; /*start from stk_src */\
+					const char *__STR_SETTER_METAMETHOD__=byte_code_metamethod_to_symbol_str(BYTE_CODE_METAMETHOD_SET);\
+					const char *__STR_AKA_SETTER_METAMETHOD__=byte_code_metamethod_to_operator_str(BYTE_CODE_METAMETHOD_SET);\
+					*stk_arg=*stk_src;\
+					StackMemberProperty *	stk_mp=NULL;\
+					if(stk_dst->properties & STK_PROPERTY_MEMBER_PROPERTY){
+						so_aux=stk_mp->so_object;\
+					}else{
+						so_aux=(ScriptObjectClass *)stk_dst->value;
+					}
+					//StackMemberProperty *	stk_mp=NULL;\
+					//if((store_lst_setter_functions == NULL) && (stk_dst->properties & STK_PROPERTY_MEMBER_PROPERTY)){ /* try member property...*/\
+					//	stk_mp=(StackMemberProperty *)stk_dst->value;\
+						/*if(stk_mp->member_property->metamethod_members.setters.count > 0){\
+							store_lst_setter_functions=&stk_mp->member_property->metamethod_members.setters;\
+							so_aux=stk_mp->so_object;\
+						}\*/
+					//}\
+					/*if(store_lst_setter_functions == NULL){\
+						VM_STOP_EXECUTE("%s '%s' not implements metamethod add_set (aka '+='') " \
+								,stk_var->properties & STK_PROPERTY_MEMBER_PROPERTY?"Member property":"Symbol" \
+								,SFI_GET_SYMBOL_NAME(calling_function,instruction-1)\
+						);\
+					}\*/
+					ScriptFunction *ptr_function_found=(ScriptFunction *)(((Symbol *)(((StackElement *)(store_lst_setter_functions->items[0]))->value))->ref_ptr);\
+					if(so_aux->isNativeObject()){ /* because object is native, we can have more than one _setter */ \
+						if((ptr_function_found=vm_find_function( \
+								vm \
+								,data->script_type_factory->getScriptType(so_aux->idx_script_type)\
+								,calling_function\
+								,instruction\
+								,false\
+								,stk_mp==NULL?"_set":(ZS_SYMBOL_NAME_MEMBER_PROPERTY_METAMETHOD_SETTER(BYTE_CODE_METAMETHOD_ADD_SET,stk_mp->member_property->property_name)).c_str() /* symbol to find */\
+								,stk_arg \
+								,1))==NULL){ \
+							if(stk_dst->properties & STK_PROPERTY_MEMBER_PROPERTY){ \
+								VM_STOP_EXECUTE("Property '%s::%s' does not implement metamethod '%s'"\
+										,so_aux->getScriptType()->script_type_name.c_str()\
+										,stk_mp->member_property->property_name.c_str()\
+										,__STR_SETTER_METAMETHOD__\
+								);\
+							}else{\
+								VM_STOP_EXECUTE("Class '%s' does not implement '%s' metamethod" \
+										,so_aux->getScriptType()->script_type_name.c_str() \
+										,__STR_SETTER_METAMETHOD__\
+								);\
+							}\
+						}\
+					}else if(store_lst_setter_functions->count>1){ /* it has overrided metamethods */\
+						Symbol * symbol_setter = so_aux->getScriptType()->getSymbol(__STR_SETTER_METAMETHOD__); \
+						if(symbol_setter == NULL){\
+							VM_STOP_EXECUTE("Operator metamethod '%s' (aka %s) is not implemented"\
+									,__STR_SETTER_METAMETHOD__\
+									,__STR_AKA_SETTER_METAMETHOD__\
+							);\
+						}\
+						if((symbol_setter->properties & FUNCTION_PROPERTY_MEMBER_FUNCTION)==0){\
+							VM_STOP_EXECUTE("Operator metamethod '%s' (aka %s) is not function",__STR_SETTER_METAMETHOD__,__STR_AKA_SETTER_METAMETHOD__);\
+						}\
+						ptr_function_found=(ScriptFunction *)symbol_setter->ref_ptr;\
+					}\
+					if(ptr_function_found->properties & FUNCTION_PROPERTY_C_OBJECT_REF){\
+						vm_call_function_native(\
+								vm\
+								,so_aux\
+								,ptr_function_found\
+								,calling_function\
+								,instruction\
+								,stk_arg\
+								,1\
+						);\
+					}else{\
+						vm_call_function_script(\
+							vm\
+							,so_aux\
+							,ptr_function_found\
+							,stk_arg\
+						);\
+					}\
+					data->stk_vm_current=stk_vm_start;
+
 				}else{ // store through script assignment
 
 					if(STK_IS_SCRIPT_OBJECT_VAR_REF(stk_src)){
