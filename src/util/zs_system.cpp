42bbdcef35233a1f91fe4f52395b07a6274c6320
jespada <jespa007@gmail.com>
2022-11-10 10:49:58 +0100

    last changes

--- src/util/zs_system.cpp
+++ src/util/zs_system.cpp
@@ -4,12 +4,14 @@
  */
 #include "zetscript.h"
 
+#define ZS_ONE_OVER_NANOS_PER_SECONDS	(1.0/1000000)
+
 namespace zetscript{
 	namespace zs_system{
 		static zs_float __clock_last_ms__=0;
 
 		zs_float clock(){
-			zs_float current_ms=(1.0/1000000.0f)*std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+			zs_float current_ms=ZS_ONE_OVER_NANOS_PER_SECONDS*std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 			zs_float diff=0;
 			if(__clock_last_ms__!=0){
 				diff=current_ms-__clock_last_ms__;
