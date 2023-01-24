ea1db1a99df119d138558b72bb952a55b2a2236c
jespada <jespa007@gmail.com>
2023-01-24 21:42:01 +0100

    last changes

--- doc/build_examples.sh
+++ doc/build_examples.sh
@@ -4,15 +4,11 @@ dirs=("string")
 
 for i in ${dirs[@]}
 do
-	echo $i
-	echo "examples/the_language/"$i"/*.zs"
 	for j in "examples/the_language/"$i"/*.zs"
 	do
-		echo $j
 		base_file="$(basename $j .zs)"
-		echo $base_file
-		#echo "examples/the_language/"$i"/"$base_file"_out.txt"
-		#../build/gcc/Release/zs --no-execution-time "examples/the_language/"$i"/"$base_file".zs" > "examples/the_language/"$i"/"$base_file"_out.txt"
+		echo "$base_file"
+		../build/gcc/Release/zs --no-execution-time "examples/the_language/"$i"/"$base_file".zs" > "examples/the_language/"$i"/"$base_file"_out.txt"
 		
 	done
 done
