2bd387a82d61dc853694eb01e1290bfd90cdbf8a
jespada <jespa007@gmail.com>
2023-03-28 17:30:02 +0200

    last changes

---
+++ doc/examples/the_api/bind/bind_function_param_custom_type_object.cpp
@@ -0,0 +1,42 @@
+#include "zetscript.h"
+
+using zetscript::ZetScript;
+using zetscript::ObjectScriptObject;
+using zetscript::zs_int;
+using zetscript::zs_float;
+using zetscript::zs_map;
+
+// c function expects an array of integers and floats
+void funParamObjectScriptObject(ZetScript *_zs, ObjectScriptObject *_object){
+	printf("Values in object:\n");
+	zs_map *map=_object->getMapFields();
+	for(auto it=map->begin(); !it.end();it.next()){
+        printf(
+			"Key: '%s' => Value: %i\n"
+			,it.key
+			,(int)_object->get<zs_int>(it.key)
+		);
+    }
+}
+
+int main(){
+	ZetScript zs;
+
+    zs.bindFunction("funParamObjectScriptObject",funParamObjectScriptObject);
+
+    // call c function with string param
+    zs.eval(
+        "funParamObjectScriptObject({"
+        "    a:0"
+        "     ,b:1"
+        "     ,c:2"
+        "     ,d:3"
+        "     ,e:4"
+
+        "});"
+ 	);
+
+
+ 	return 0;
+}
+
