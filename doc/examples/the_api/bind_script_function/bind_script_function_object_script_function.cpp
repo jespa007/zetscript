d5079a457e37b01a723e6fbccf49bf0410ff3bdd
Jordi Espada <jespa007@gmail.com>
2023-05-29 23:57:39 +0200

    =last changes

---
+++ doc/examples/the_api/bind_script_function/bind_script_function_object_script_function.cpp
@@ -0,0 +1,25 @@
+#include "zetscript.h"
+#include <iostream>
+
+using zetscript::zs_float;
+
+int main()
+{
+	zetscript::ZetScript zs;
+
+    zs.eval(
+ 		"class A{\n"
+        "    print(){\n"
+        "       Console::outln(\"Call from instanced object\")"
+        "   }\n"
+        "}\n"
+        "var a=new A();\n"
+ 	);
+
+    auto a_print=zs.bindScriptFunction<void(void)>("a.print");
+
+    a_print();
+
+ 	return 0;
+}
+
