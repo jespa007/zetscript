10b7243c476c21dc7fe0836bfeebc92f0d342e65
jespada <jespa007@gmail.com>
2023-07-27 13:33:08 +0200

    =last changes

--- doc/examples/the_api/register_type/register_member_property_metamethod_xorset.cpp
+++ doc/examples/the_api/register_type/register_member_property_metamethod_xorset.cpp
@@ -8,11 +8,8 @@ int main(){
 	
 	zs.eval(
 		"var number=new Number(0x0);\n"
-		"Console::outln(\"number.value => \"+number.value)\n"
-		"number.value^=0xa;\n"
-		"Console::outln(\"number.value^=0xa => \"+number.value)\n"
-		"number.value^=new Number(0x9);\n"
-		"Console::outln(\"number.value^=new Number(0x9) => \"+number.value)\n"
+		"Console::outln(\"number.value => {0}\",number.value)\n"
+		"Console::outln(\"number.value^=0xa => {0}\",number.value^=0xa)\n"
 	);
 
 	return 0;
