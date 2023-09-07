7d0ed25fe400667608d33c1efc7abf467264be37
jespada <jespa007@gmail.com>
2023-09-07 17:11:37 +0200

    reorganize

--- doc/examples/make.sh
+++ doc/examples/make.sh
@@ -7,16 +7,16 @@ then
 	EXTENSION_EXE=".exe"
 fi
 
-the_language_example_dirs=(console integer float string function array object class type loop math json system datetime)
+language_example_dirs=(console integer float string function array object class type loop math json system datetime)
 
 # build output for language examples
-for i in "${the_language_example_dirs[@]}"
+for i in "${language_example_dirs[@]}"
 do
-	dir="the_language/"$i"/*.zs"
+	dir="lang/"$i"/*.zs"
 	for j in $dir
 	do
 		base_file="$(basename $j .zs)"
-		../../bin/gcc/Release/zs --no-execution-time "the_language/"$i"/"$base_file".zs" > "the_language/"$i"/"$base_file"_out.txt" 
+		../../bin/gcc/Release/zs --no-execution-time "lang/"$i"/"$base_file".zs" > "language/"$i"/"$base_file"_out.txt" 
 		
 	done
 done
@@ -24,33 +24,33 @@ done
 
 #build output for api examples
 
-if ! [ -d the_api/build ] 
+if ! [ -d api/build ] 
 then
-	mkdir -p the_api/build
+	mkdir -p api/build
 fi
 
-the_api_example_dirs=(eval bind_script_function register_function builtin_type register_type)
+api_example_dirs=(eval bind_script_function register_function builtin_type register_type)
 
-dir_zs="the_api/eval/*.zs"
+dir_zs="api/eval/*.zs"
 for j in $dir_zs
 do
 	base_file="$(basename $j .zs)"
-	cp "the_api/eval/"$base_file".zs" "the_api/build/"$base_file".zs"
+	cp "api/eval/"$base_file".zs" "api/build/"$base_file".zs"
 done
 
-for i in "${the_api_example_dirs[@]}"
+for i in "${api_example_dirs[@]}"
 do
 
-	dir_cpp="the_api/"$i"/*.cpp"
+	dir_cpp="api/"$i"/*.cpp"
 	for j in $dir_cpp
 	do
 		base_file="$(basename $j .cpp)"
 		echo "processing "$base_file".cpp"
-		g++ -std=gnu++0x -Wall -pedantic -O2 "the_api/"$i"/"$base_file".cpp" ../../bin/gcc/Release/libzetscript-2-0-0-static.a -o "the_api/build/"$base_file$EXTENSION_EXE -I ../../src
+		g++ -std=gnu++0x -Wall -pedantic -O2 "api/"$i"/"$base_file".cpp" ../../bin/gcc/Release/libzetscript-2-0-0-static.a -o "api/build/"$base_file$EXTENSION_EXE -I ../../src
 		if [ $? -eq 0 ] 
 		then
 			current=$PWD
-			cd "the_api/build"
+			cd "api/build"
 			./$base_file > "../"$i"/"$base_file"_out.txt"
 			cd $current
 		fi
