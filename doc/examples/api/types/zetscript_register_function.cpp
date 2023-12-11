a4cda9aedfd38375ad34e5c0417e0d49b73f92d2
jespada <jespa007@gmail.com>
2023-12-11 19:37:01 +0100

    =last changes

--- doc/examples/api/types/zetscript_register_function.cpp
+++ doc/examples/api/types/zetscript_register_function.cpp
@@ -1,7 +1,23 @@
 #include "zetscript.h"
-int main(){
-	zetscript::ZetScript zs;
 
-	
-	return 0;
+using zetscript::ZetScript;
+
+// ZetScript C++ interface function
+void sayHelloWorld(ZetScript *_zs){
+    printf("Hello world\n");
 }
+
+int main(){
+	ZetScript zs;
+
+	// Registers sayHelloWorld as 'sayHelloWorld' symbol name
+    zs.registerFunction("sayHelloWorld",sayHelloWorld);
+
+    // Evaluates a script where it calls 'sayHelloWorld' function
+    zs.compileAndRun(
+        "sayHelloWorld();"
+ 	);
+
+    return 0;
+}
+
